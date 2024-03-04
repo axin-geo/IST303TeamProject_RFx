41cd191b4bf5fd9e20ca8aec058e78b40537161d
Aote Xin <axin@esri.com>
2024-03-03 19:15:31 -0800

    create rfx_info_to_json.py

---
+++ rfx_info_to_json.py
@@ -0,0 +1,12 @@
+import os
+# import arcpy ## installing Arcpy requires Pro to be installed 
+
+# dump out to json file
+
+
+if not os.path.exists("rfx_info"):
+    os.mkdir("rfx_info")
+
+## arcgis version (Product name, version number)
+## raster functions45
+import json
